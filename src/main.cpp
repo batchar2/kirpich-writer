1179e127a9a526d49b7dc6687952566157472b8b
Skokov Stanislav <sk@vizorlabs.ru>
2020-11-14 00:13:42 +0700

    second commit

--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,14 @@ static bool is_running = true;
 int main()
 {
     auto http_server = k::http::server::instance();
-//    auto archiving_manager = k::archiving::manager::instance();
-
-
-//    while (is_running) { // просто ждем пока сервер работает, заменить на сигнал или кондвар
+    auto archiving_manager = k::archiving::manager::instance();
+    
+    while (is_running) { // просто ждем пока сервер работает, заменить на сигнал или кондвар
         std::this_thread::sleep_for(std::chrono::milliseconds(300));
-//    }
+    }
 
     http_server->stop();
-
-//    (void)archiving_manager;
+    archiving_manager->stop();
 
     return 0;
 }
