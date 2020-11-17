34ec0ab9ff4f00eb7621096597dc8a835788945a
batchar2 <skokov.stanislav@yandex.ru>
2020-11-17 09:55:49 +0700

    archiving test

--- test/archiving/archiving.cpp
+++ test/archiving/archiving.cpp
@@ -10,35 +10,35 @@
 
 BOOST_AUTO_TEST_CASE(start_stop_manager)
 {
-//    auto manager = k::archiving::manager::instance();
-//
-//    std::this_thread::sleep_for(std::chrono::seconds(1));
-//
-//    BOOST_CHECK_EQUAL(manager->is_running(), true);
-//
-//    manager->stop();
-//
-//    BOOST_CHECK_EQUAL(manager->is_running(), false);
+   auto manager = k::archiving::manager::instance();
+
+   std::this_thread::sleep_for(std::chrono::seconds(1));
+
+   BOOST_CHECK_EQUAL(manager->is_running(), true);
+
+   manager->stop();
+
+   BOOST_CHECK_EQUAL(manager->is_running(), false);
 }
 
 
 BOOST_AUTO_TEST_CASE(start_camera)
 {
-//    ::setenv("ARCHIVE_DURATION_SEC","2",1);
-//
-//    auto manager = k::archiving::manager::instance();
-//
-//    std::this_thread::sleep_for(std::chrono::seconds(1));
-//
-//    BOOST_CHECK_EQUAL(manager->is_running(), true);
-//
-//    manager->camera_start("1", "rtsp://stream.studio360.tv:554/nw/nw_576p");
-//
-//    std::this_thread::sleep_for(std::chrono::seconds(3));
-//
-//    BOOST_CHECK_EQUAL((std::filesystem::exists("camera-1.csv")), true);
-//
-//    manager->stop();
-//
-//    BOOST_CHECK_EQUAL(manager->is_running(), false);
+   ::setenv("ARCHIVE_DURATION_SEC","2",1);
+
+   auto manager = k::archiving::manager::instance();
+
+   std::this_thread::sleep_for(std::chrono::seconds(1));
+
+   BOOST_CHECK_EQUAL(manager->is_running(), true);
+
+   manager->camera_start("1", "rtsp://stream.studio360.tv:554/nw/nw_576p");
+
+   std::this_thread::sleep_for(std::chrono::seconds(3));
+
+   BOOST_CHECK_EQUAL((std::filesystem::exists("camera-1.csv")), true);
+
+   manager->stop();
+
+   BOOST_CHECK_EQUAL(manager->is_running(), false);
 }
